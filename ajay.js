42ab0b115a397e255985f80f4bfddbbe82e196ac
Geoffrey Nganga <ngangageoffry994@gmail.com>
2024-03-20 09:54:37 +0300

    first commit - committing all files to the repository

--- ajay.js
+++ ajay.js
@@ -1,7 +1,29 @@
-const fire = document.querySelector("fire")
 
-window.addEventListener("scroll", function(){
-    fire.classList.toggle("sticky",window.scrollY > 200)
-}
-)
+function submitForm() {
+    document.getElementById("findme").submit();
+  }
 
+  const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) =>{
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+  });
+
+
+  const hiddenElements = document.querySelectorAll('.hidden');
+  hiddenElements.forEach((el) => observer.observe(el));
+
+  function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display ='flex'
+  }
+
+  function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display ='none'
+  }
